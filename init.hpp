d7047341cd250fd3e37b5b369f56114b21666b5a
Jerry <920859702@qq.com>
2017-02-15 12:59:05 +0800

    add issue 4

---
+++ init.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "init.cpp"
+using namespace std;
+#define SIZE 1024 
+int jerry_rand(void);
+void jerry_srand(unsigned int seed);
+void init_array_rand(char shuzu[],long long size);
+
---
+++ print.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "print.cpp"
+using namespace std;
+void print_binary_file(char shuzu[],long long size);
+void print_array_to_file(char shuzu[],long long size);
+
